bb3db3e10a1c106ede834b1d3325edd19bb11483
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-12 02:48:05 +0600

    update:Jobs schema

--- src/Schema/jobSchema.ts
+++ src/Schema/jobSchema.ts
@@ -1,44 +1,68 @@
 import { Schema } from "mongoose";
+const ObjectId = Schema.Types.ObjectId;
 
-const jobSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "Please enter job title"],
-    trim: true,
-    maxLength: [100, "Job title cannot exceed 100 characters"],
-    minLength: [3, "Job title cannot be less than 3 characters"],
-    unique: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  jobype: {
-    type: String,
-    required: true,
-    enum: {
-      values: ["full-time", "part-time", "contract"],
+const jobSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please enter job title"],
+      trim: true,
+      maxLength: [100, "Job title cannot exceed 100 characters"],
+      minLength: [3, "Job title cannot be less than 3 characters"],
     },
-    location: {
+    description: {
+      type: [String],
+      required: true,
+    },
+    jobType: {
       type: String,
       required: true,
       enum: {
-        values: ["remote", "on-site"],
-        message:
-          "Please select the correct location for job, it must be either remote or on-site",
+        values: ["full-time", "part-time", "contract","internship"],
+      },
+      location: {
+        type: String,
+        required: true,
+        enum: {
+          values: ["remote", "on-site"],
+          message:
+            "Please select the correct location for job, it must be either remote or on-site",
+        },
+      },
+      status: {
+        type: String,
+        required: true,
+        enum: {
+          values: ["open", "closed"],
+          message:
+            "Please select the correct status for job, it must be either open or closed",
+        },
+        default: "open",
       },
     },
-    status: {
-      type: String,
-      required: true,
-      enum: {
-        values: ["open", "closed"],
-        message:
-          "Please select the correct status for job, it must be either open or closed",
+    hiringCompany: {
+      name: {
+        type: String,
+        required: true,
+      },
+      id: {
+        type: ObjectId,
+        ref: "Company",
+      },
+    },
+    hiringManager: {
+      name: {
+        type: String,
+        required: true,
+      },
+      id: {
+        type: ObjectId,
+        ref: "User",
       },
     },
   },
-},{
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 export default jobSchema;
--- src/controllers/jobController.ts
+++ src/controllers/jobController.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
+import { JobModel } from "../models/jobModel";
 
 // post data
 const createJob = async (req: Request, res: Response) => {
-  res.send("It's working! Yayyy");
+  try {
+    const job = await JobModel.create(req.body);
+    res.status(201).send(job);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
 
 export const jobRouter = { createJob };
