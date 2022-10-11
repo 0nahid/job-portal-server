e6be3aef8556703a6755524e444d1e9212ee394f
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-12 02:47:23 +0600

    added hiringCompany schema

---
+++ src/Schema/hiringCompanySchema.ts
@@ -0,0 +1,55 @@
+import { Schema } from "mongoose";
+import validator from "validator";
+const ObjectId = Schema.Types.ObjectId;
+
+const higherCompanySchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: [100, "Company name cannot exceed 100 characters"],
+    minLength: [3, "Company name cannot be less than 3 characters"],
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+    required: true,
+    validate: [validator.isURL, "Please enter a valid URL"],
+  },
+  logo: {
+    type: String,
+    required: true,
+    validate: [validator.isURL, "Please enter a valid URL"],
+    default: "https://via.placeholder.com/150",
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    validate: [validator.isMobilePhone, "Please enter a valid phone number"],
+  },
+  hiringManager: {
+    name: String,
+    id: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  jobs: {
+    type: [ObjectId],
+    ref: "Job",
+  },
+});
