9cabe5a6bdbec9487a1d0284c0a4a4469b8406ba
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-12 13:19:44 +0600

    jwt with userName,email & role

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -4,8 +4,8 @@ import { IUser } from "../Schema/userSchema";
 const JWT_SECRET = process.env.JWT_SECRET || "";
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "";
 export const generateToken = (user: IUser) => {
-  const { email, role } = user;
-  const token = jwt.sign({ email, role }, JWT_SECRET, {
+  const { email, role, userName } = user;
+  const token = jwt.sign({ email, role, userName }, JWT_SECRET, {
     expiresIn: JWT_EXPIRES_IN,
   });
 
