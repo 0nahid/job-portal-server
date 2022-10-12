8c0dd4b067a6f1c522f6f2e92131580d62b9596b
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-12 20:50:42 +0600

    get single jobs

--- src/controllers/jobController.ts
+++ src/controllers/jobController.ts
@@ -14,13 +14,31 @@ const createJob = async (req: Request, res: Response) => {
 // get data
 const getJobs = async (req: Request, res: Response) => {
   try {
-    const job = await JobModel.find({})
-      .populate("hiringManager.id", "-password")
-      .populate("hiringCompany.id");
-    res.status(200).send(job);
+    const job = await JobModel.find({}).populate("hiringCompany.id");
+    res.status(200).send({
+      message: "All jobs",
+      status: 200,
+      data: job,
+    });
   } catch (error) {
     res.status(400).send(error);
   }
 };
 
-export const jobRouter = { createJob, getJobs };
+// get single data
+const getSingleJob = async (req: Request, res: Response) => {
+  try {
+    const job = await JobModel.findById(req.params.id)
+      .populate("hiringManager.id", "-password")
+      .populate("hiringCompany.id");
+    res.status(200).send({
+      message: "Single job",
+      status: 200,
+      data: job,
+    });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const jobRouter = { createJob, getJobs, getSingleJob };
--- src/routes/v1/jobRouter.ts
+++ src/routes/v1/jobRouter.ts
@@ -4,5 +4,6 @@ const router: Router = Router();
 
 router.post("/", jobRouter.createJob);
 router.get("/", jobRouter.getJobs);
+router.get("/:id", jobRouter.getSingleJob); 
 
 export default router;
